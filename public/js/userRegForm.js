bc6ff5c3b647f8840476088148c31cd8571aa403
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-04-30 15:29:26 +0530

    user register form done

---
+++ public/js/userRegForm.js
@@ -0,0 +1,13 @@
+// checking user type
+document.getElementsByClassName('user_section')[0]
+    .addEventListener('click', function (event) {
+        if (document.getElementById('user_student').checked) {
+            console.log("STUDENT");
+            document.getElementById("teacherSection").style.display = "none";
+            document.getElementById("studentSection").style.display = "block";
+        } else if (document.getElementById('user_teacher').checked) {
+            console.log("teACHER");
+            document.getElementById("studentSection").style.display = "none";
+            document.getElementById("teacherSection").style.display = "block";
+        }
+    });
