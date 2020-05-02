a5a70d59ee8b4e6aaa08fa26976f2e8d15309718
Ashan Shanaka <jpashanshanaka@gmail.com>
2020-05-02 22:09:53 +0530

    User registration completed

--- public/js/userRegForm.js
+++ public/js/userRegForm.js
@@ -1,12 +1,14 @@
+document.getElementById("teacherSection").style.display = "none";
+
 // checking user type
 document.getElementsByClassName('user_section')[0]
     .addEventListener('click', function (event) {
         if (document.getElementById('user_student').checked) {
-            console.log("STUDENT");
+            // console.log("STUDENT");
             document.getElementById("teacherSection").style.display = "none";
             document.getElementById("studentSection").style.display = "block";
         } else if (document.getElementById('user_teacher').checked) {
-            console.log("teACHER");
+            // console.log("teACHER");
             document.getElementById("studentSection").style.display = "none";
             document.getElementById("teacherSection").style.display = "block";
         }
--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,131 @@ const passport = require('passport');
 // Load User Model
 require('../models/User');
 const User = mongoose.model('users');
+// Load Student Model
+require('../models/Student');
+const Student = mongoose.model('students');
+// Load Teacher Model
+require('../models/Teacher');
+const Teacher = mongoose.model('teachers');
 
-// User Register Route
+// User GET Register Route
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
+// User Register POST Route
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if (req.body.password != req.body.conf_password) {
+        errors.push({ text: 'Passwords do not match with each other' });
+    }
+
+    if (req.body.password.length < 4) {
+        errors.push({ text: 'Password must be at least 4 characters' });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+            tel: req.body.tel,
+            userType: req.body.userType,
+            district: req.body.district,
+            school: req.body.school,
+            dob: req.body.dob,
+            subject: req.body.subject,
+            nic: req.body.nic,
+            password: req.body.password,
+            conf_password: req.body.conf_password
+        });
+    } else {
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'Email already regsitered');
+                    res.redirect('/users/register');
+                } else {
+
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        address: req.body.address,
+                        tel: req.body.tel,
+                        userType: req.body.userType,
+                        password: req.body.password
+                    });
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    if (user.userType == 'student') {
+                                        const newStudent = new Student({
+                                            district: req.body.district,
+                                            school: req.body.school,
+                                            dob: req.body.dob,
+                                            userId: user.id
+                                        });
+                                        newStudent.save()
+                                            .then(student => {
+                                                req.flash('success_msg', 'You are now registered and can log in');
+                                                res.redirect('/users/login');
+                                            })
+                                            .catch(err => {
+                                                console.log(err);
+                                                return;
+                                            });
+                                    } else {
+                                        const newTeacher = new Teacher({
+                                            subject: req.body.subject,
+                                            nic: req.body.nic,
+                                            userId: user.id
+                                        });
+                                        newTeacher.save()
+                                            .then(teacher => {
+                                                req.flash('success_msg', 'You are now registered and can log in');
+                                                res.redirect('/users/login');
+                                            })
+                                            .catch(err => {
+                                                console.log(err);
+                                                return;
+                                            });
+                                    }
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return;
+                                });
+                        });
+                    });
+                }
+            });
+    }
+});
+
+//   // Edit User PUT process
+//   router.put('/edit/:id', (req, res) => {
+//     User.findOne({
+//         _id: req.params.id
+//     })
+//         .then(user => {
+//             // new values
+//             user.name = req.body.name;
+//             user.email = req.body.email;
+//             user.amountPerKG = req.body.amountPerKG;
+
+//             user.save()
+//                 .then(user => {
+//                     req.flash('success_msg', 'User data updated');
+//                     res.redirect('/');
+//                 })
+//         });
+//   });
+
 
 
 // User Login Route
